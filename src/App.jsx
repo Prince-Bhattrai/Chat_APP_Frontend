28dce77b395230e5e48457f825f933d5a1c72206
Prince Bhattrai <bhattraiprince39@gmail.com>
2025-06-08 14:07:24 +0545

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from "socket.io-client";
 import "./App.css"
-const socket = io("http://localhost:4000");
+const socket = io("https://chat-app-backend-p26f.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
